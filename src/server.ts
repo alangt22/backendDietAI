eeaed18ef3fb48d70729993e07f6948a50c5af19
alangt22 <alansilva2896@gmail.com>
2025-10-03 14:48:30 -0300

    Backend

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import cors from '@fastify/cors'
+import Fastify from 'fastify'
+import { planRoutes } from './routes/plan'
+
+const app = Fastify({
+    logger: true,
+})
+
+await app.register(cors, {
+    origin: "*",
+    methods: ["GET", "POST"],
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello WORLD from Fastify')
+})
+
+app.register(planRoutes)
+
+app.listen({port: Number(process.env.PORT) || 3333, host: '0.0.0.0'})
+.then(() => console.log('server running port 3333'))
+.catch((err) => {
+    app.log.error(err)
+    process.exit(1)
+})
+
+
+
